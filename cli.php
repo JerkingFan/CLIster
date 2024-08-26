5edb80db2d6cb6dd757b8e9050f267a890a5ac4b
Arseny <kulbakocnt@gmail.com>
2024-08-26 21:15:43 +0300

    rename folder, restruct cli.php

--- cli.php
+++ cli.php
@@ -1,23 +1,31 @@
 <?php
 
-require 'src/CLI.php';
-require 'src/Command.php';
-require 'src/Console.php';
+require __DIR__ . '/src/CLI.php';
+require __DIR__ . '/src/Command.php';
+require __DIR__ . '/src/Console.php';
 
 // Подключаем команды
-require 'src/Command/HelloCommand.php';
-require 'src/Command/ListCommand.php';
-require 'src/Command/HelpCommand';
+require __DIR__ . '/src/Commands/HelloCommand.php';
+require __DIR__ . '/src/Commands/ListCommand.php';
+require __DIR__ . '/src/Commands/HelpCommand.php';
+require __DIR__ . '/src/Commands/MakeCommand.php';
+require __DIR__ . '/src/Commands/MigrateCommand.php';
+require __DIR__ . '/src/Commands/GreetCommand.php';
+require __DIR__ . '/src/Commands/ConfigCommand.php';
+require __DIR__ . '/src/Commands/ServeCommand.php';
 
 // Создаем экземпляр CLI
 $cli = new CLI();
 
 // Регистрируем команды
-$cli->register('help', new HelpCommand($cli));
 $cli->register('hello', new HelloCommand());
 $cli->register('list', new ListCommand());
+$cli->register('help', new HelpCommand($cli));
+$cli->register('make:command', new MakeCommand());
+$cli->register('migrate', new MigrateCommand());
+$cli->register('greet', new GreetCommand());
+$cli->register('config', new ConfigCommand());
+$cli->register('serve', new ServeCommand());
 
 // Запускаем CLI
 $cli->run();
-
-?>
