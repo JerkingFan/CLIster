61e6eeb45673a3274127726fee0bfd437a41924b
Arseny <kulbakocnt@gmail.com>
2024-08-26 21:11:49 +0300

    generate the structure for new command

---
+++ src/Command/MakeCommand.php
@@ -0,0 +1,34 @@
+<?php
+
+class MakeCommand extends Command
+{
+    public function execute($arguments)
+    {
+        $name = $arguments[0] ?? null;
+
+        if ($name === null) {
+            echo "Please provide a name for the new command.\n";
+            return;
+        }
+
+        $commandFile = __DIR__ . "/Commands/{$name}Command.php";
+
+        if (file_exists($commandFile)) {
+            echo "Command {$name} already exists.\n";
+            return;
+        }
+
+        $template = "<?php\n\nclass {$name}Command extends Command\n{\n    public function execute(\$arguments)\n    {\n        // Your code here\n    }\n\n    public function getDescription()\n    {\n        return \"Description for {$name} command.\";\n    }\n}\n";
+
+        file_put_contents($commandFile, $template);
+
+        echo "Command {$name} created successfully at {$commandFile}.\n";
+    }
+
+    public function getDescription()
+    {
+        return "Generates a new command file.";
+    }
+}
+
+?>
